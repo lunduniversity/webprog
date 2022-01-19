7023e3bfeb010a8d0a02fdd4b5a1b7f9a9ae430c
Per <per.andersson@cs.lth.se>
2022-01-19 09:28:26 +0100

    updated slides

--- labs/lab2/inventory.ES6.js
+++ labs/lab2/inventory.ES6.js
@@ -54,7 +54,7 @@ let inventory = {
 
 // recursively freeze the data structure.
 (function() {
-  deepFreeze(inventory);
+  deepFreeze(exports.inventory);
   function deepFreeze(obj) {
       Object.keys(obj).map(prop => deepFreeze(obj[prop]));
       Object.freeze(obj);
