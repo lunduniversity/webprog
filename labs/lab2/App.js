583caa6258923be1962159cae3fee69bae4d7d95
Per <per.andersson@cs.lth.se>
2023-01-29 17:19:19 +0100

    minor corrections

---
+++ labs/lab2/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import inventory from './inventory.ES6';
+
+function App()
+{
+  let extras = Object.keys(inventory).filter(name => inventory[name].extra);
+  return (
+    <div className="container py-4">
+    <header className="pb-3 mb-4 border-bottom">
+      <span className="fs-4">Min egen salladsbar</span>
+    </header>
+
+    <div className="continer col-12">
+      <div className="row h-200 p-5 bg-light border rounded-3">
+        <h2>Välj innehållet i din sallad</h2>
+          {extras.map(name => <div key={name} className="col-4">{name}</div>)}
+      </div>
+    </div>
+
+    <footer className="pt-3 mt-4 text-muted border-top">
+      EDAF90 - webprogrammering
+    </footer>
+  </div>
+  );
+  }
+
+export default App;
---
+++ labs/lab2/ComposeSalad.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function ComposeSalad(props) {
+  let extras = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+  const [foundation, setFoundation] = useState('Pasta'); 
+  const [extra, setExtra] = useState({Bacon: true, Fetaost: true}); 
+
+  return (
+    <div className="container col-12">
+      <div className="row h-200 p-5 bg-light border rounded-3">
+        <h2>Välj bas</h2>
+        {extras.map(name => <div key={name} className="col-4">{name}</div>)}
+      </div>
+    </div>
+  );
+}
+export default ComposeSalad;
