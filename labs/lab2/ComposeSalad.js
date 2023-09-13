9314d3098fbaa09682cb7102b45636b62ec55e8b
Per <per.andersson@cs.lth.se>
2023-09-13 13:48:57 +0200

    fixed typo

--- labs/lab2/ComposeSalad.js
+++ labs/lab2/ComposeSalad.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
 function ComposeSalad(props) {
-  const extras = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+  const foundationList = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
   const [foundation, setFoundation] = useState('Pasta');
-  const [extra, setExtra] = useState({ Bacon: true, Fetaost: true });
+  const [extras, setExtra] = useState({ Bacon: true, Fetaost: true });
 
   return (
     <div className="continer col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
         <h2>Välj bas</h2>
-        {extras.map(name => <div key={name} className="col-4">{name}</div>)}
+        {foundationList.map(name => <div key={name} className="col-4">{name}</div>)}
       </div>
     </div>
   );
